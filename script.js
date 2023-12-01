dcb98f92e340523905afb6ceaae47f0e7247cd3b
민재 유 <111565097+arpa0091@users.noreply.github.com>
2023-12-01 23:13:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 // 비밀번호 일치 여부 확인 함수
-const url = 'https://4351-123-212-234-141.ngrok-free.app';
+let url = 'https://4351-123-212-234-141.ngrok-free.app';
 document.getElementById('loginForm').addEventListener('submit', async function (e) {
     e.preventDefault();
 
