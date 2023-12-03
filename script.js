2a4466476685d46099b455a5001845b823d9d2c0
민재 유 <111565097+arpa0091@users.noreply.github.com>
2023-12-03 13:49:25 +0900

    a

--- script.js
+++ script.js
@@ -153,7 +153,6 @@ document.getElementById('st_btn').addEventListener('click', async () => {
 
         if (response.status === 200) {
             // 서버로부터 사용자 정보를 받았을 때의 처리
-            alert('로그인된 사용자');
             href = '/study.html';
         } else if (response.status === 401) {
             // 로그인되지 않은 상태일 때의 처리
