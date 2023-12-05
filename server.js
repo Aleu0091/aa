c97cf5881f0e0dae6117350f7984d62b7de0ce17
arpa0091 <111565097+arpa0091@users.noreply.github.com>
2023-12-05 18:02:41 +0900

    server.js 업데이트

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ const app = express();
 
 // MongoDB 연결 설정
 const uri = 'mongodb+srv://ueged13:VmNMiFeGheGzPZPl@cluster0.zzctp0t.mongodb.net/?retryWrites=true&w=majority';
+
 const client = new MongoClient(uri);
-
 let usersCollection;
-
 async function connectToMongo() {
     try {
         await client.connect();
@@ -23,6 +22,7 @@ async function connectToMongo() {
         console.error('Error connecting to MongoDB:', err);
     }
 }
+
 const corsOptions = {
     origin: 'https://www.poayl.xyz',
     credentials: true
@@ -33,13 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 connectToMongo();
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://www.poayl.xyz');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    next();
-}); //
-// 세션 설정
+
 app.use(
     session({
         secret: 'dfgsdgjvfdslgjm',
@@ -48,12 +42,9 @@ app.use(
     })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Passport Local Strategy 설정
 passport.use(
     new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
         try {
@@ -89,7 +80,6 @@ passport.deserializeUser(async (id, done) => {
     }
 });
 
-// 회원가입 엔드포인트
 app.post('/signup', async (req, res) => {
     const { username: signupUsername, email: signupEmail, password: signupPassword } = req.body;
 
@@ -108,15 +98,13 @@ app.post('/signup', async (req, res) => {
         };
 
         const result = await usersCollection.insertOne(newUser);
-        res.status(201).json({ message: 'singup', userId: result.insertedId });
+        res.status(201).json({ message: 'signup', userId: result.insertedId });
     } catch (err) {
         res.status(500).json({ message: 'error while signup' });
     }
 });
 
-// 로그인 엔드포인트
 app.post('/login', passport.authenticate('local', { failureRedirect: '/login-failure' }), (req, res) => {
-    // Manually call req.login() to save the authenticated state
     req.login(req.user, (err) => {
         if (err) {
             return res.status(500).json({ message: 'Error during login' });
@@ -125,15 +113,14 @@ app.post('/login', passport.authenticate('local', { failureRedirect: '/login-fai
     });
 });
 
-// 로그아웃 엔드포인트
 app.post('/logout', (req, res) => {
     req.logout((err) => {
         if (err) {
             return res.status(500).json({ message: 'error while logout' });
         }
-        res.header('Access-Control-Allow-Origin', 'https://www.poayl.xyz'); // 클라이언트 도메인 설정
-        res.header('Access-Control-Allow-Credentials', true); // 인증 정보 허용
-        res.send('success logout'); // 클라이언트에게 로그아웃 성공을 응답합니다.
+        res.header('Access-Control-Allow-Origin', 'https://www.poayl.xyz');
+        res.header('Access-Control-Allow-Credentials', true);
+        res.send('success logout');
     });
 });
 
@@ -144,12 +131,10 @@ function isAuthenticated(req, res, next) {
     res.status(401).json({ error: 'you need login' });
 }
 
-// profile 라우트에 isAuthenticated 함수 적용
 app.get('/profile', isAuthenticated, (req, res) => {
     res.json(req.user);
 });
 
-// 서버 설정
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`서버가 localhost:${port}에서 실행 중입니다.`);
